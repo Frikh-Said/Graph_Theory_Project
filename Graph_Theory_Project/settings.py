ddcaf4dd9025f339ddc86e2a387350c46e75ce32
Frikh-Said <frikh.said@hotmail.com>
2023-05-28 15:06:24 +0100

    fixing some style

--- Graph_Theory_Project/settings.py
+++ Graph_Theory_Project/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Graph_Theory_app',
     'pwa',
-    'compressor',
 ]
 
 MIDDLEWARE = [
@@ -124,12 +123,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-COMPRESS_ROOT = BASE_DIR / 'static'
-
-COMPRESS_ENABLED = True
-
-STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
